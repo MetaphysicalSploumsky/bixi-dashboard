e0331399d292161c00e642f95b51112f0d61f9f7
Amine <mohammed.sahraoui@mail.mcgill.ca>
2025-11-03 10:38:04 -0500

    better map

--- dashboard.py
+++ dashboard.py
@@ -3,7 +3,7 @@
 import streamlit as st 
 from streamlit_folium import st_folium
 import polars as pl
-import folium
+import pydeck as pdk
 from millify import millify
 
 st.write("""
@@ -40,30 +40,58 @@ num_stations_less_3docks = (
 )
 percentage_overflows = num_stations_less_3docks / total_stations * 100
 c.metric("Stations at < 3 docks", f"{num_stations_less_3docks} ({percentage_overflows:.2f} %)", "5%", border=True)
+ 
 
+st.sidebar.header("Filters")
+hide_empty = st.sidebar.checkbox("Hide almost empty stations")
+hide_full = st.sidebar.checkbox("Hide almost full stations")
 
-city_centre_coords = [45.4996, -73.5668]
-m = folium.Map(location=city_centre_coords, zoom_start=14)
-# If you want to dynamically add or remove items from the map,
-# add them to a FeatureGroup and pass it to st_folium
-fg = folium.FeatureGroup(name="Stations") 
-for row in df.iter_rows(named=True):
-    if row['is functional'] == 1:
-        fg.add_child(
-        folium.Marker(
-            location=[row['lat'], row['lon']],
-            popup=row['name'],
-            icon=folium.Icon(color="red") if row['number available bikes'] <= 5 else folium.Icon(color="green")
-        )
-    )
+df_filtered = df.filter(pl.col("is functional") == 1)
 
-out = st_folium(
-    m,
-    feature_group_to_add=fg,
-    zoom=17,
-    center=(45.4996, -73.5668),
-    width=1200,
-    height=500,
+if hide_empty:
+    df_filtered = df_filtered.filter(pl.col("number available bikes") >= 3)
+if hide_full:
+    df_filtered = df_filtered.filter(pl.col("number available docks") >= 3)
+
+data = df_filtered.to_pandas()
+
+def color_row(row):
+    if row["number available bikes"] < 3:
+        return [255, 75, 75]  # red
+    elif row["number available docks"] < 3:
+        return [255, 200, 0]  # yellow 
+    else:
+        return [0, 180, 0]  # green normal
+
+data["color"] = data.apply(color_row, axis=1)
+
+layer = pdk.Layer(
+    "ScatterplotLayer",
+    data=data,
+    get_position=["lon", "lat"],
+    get_fill_color="color",
+    get_radius=30, # extra: decrease on zoom, increase on dezoom
+    pickable=True,
+    opacity=0.8,
 )
 
-# streamlit run dashboard.py
+view_state = pdk.ViewState(latitude=45.4996, longitude=-73.5668, zoom=13)
+
+deck = pdk.Deck(
+    layers=[layer],
+    initial_view_state=view_state,
+    map_style="light",  
+    
+    tooltip={
+        'html': '{name} <br> Bikes: {number available bikes} <br> Open docks: {number available docks}',
+        'style': {
+            'color': 'white'
+        }
+    } # type: ignore
+    
+    
+)
+
+st.pydeck_chart(deck)
+
+    
