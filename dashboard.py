2bc06ef3cea8e907e390ffbfe0704313d1cf13ae
Amine <mohammed.sahraoui@mail.mcgill.ca>
2025-11-05 17:16:54 -0500

    lambda function

--- dashboard.py
+++ dashboard.py
@@ -2,13 +2,81 @@ import streamlit as st
 import polars as pl
 import pydeck as pdk
 from millify import millify
-from collections import deque
-from io import StringIO
 import pandas as pd
+import psycopg2
+from env_var import ENDPOINT, PW, USERNAME, NAME, PORT
 
 TOTAL_BIKES = 12600 # per bixi, approx.
 MIN_LAT, MAX_LAT = 45.40, 45.55 # Montreal + Longueil
 MIN_LON, MAX_LON = -73.70, -73.45
+
+@st.cache_resource
+def get_db_connection():
+    try:
+        conn = psycopg2.connect(
+            dbname=NAME, 
+            user=USERNAME, 
+            password=PW, 
+            host=ENDPOINT, 
+            port=PORT
+        )
+        return conn # cache 
+    except (Exception, psycopg2.Error) as error:
+        st.error(f"Error connecting to database: {error}")
+        return None
+
+@st.cache_data(ttl=300) # cache for 5 minutes
+def load_latest_snapshot(_conn):
+    query = """
+        SELECT * FROM station_status_log
+        WHERE fetched_at = (SELECT MAX(fetched_at) FROM station_status_log);
+    """
+    
+    df = pl.read_database(query, _conn)
+    
+    df = df.rename({
+        "is_functional": "is functional",
+        "bikes_av": "number available bikes",
+        "docks_av": "number available docks"
+    })
+    return df
+
+@st.cache_data(ttl=300) 
+def load_aggregate_history(_conn):
+
+    query = "SELECT * FROM system_aggregate_log ORDER BY fetched_at ASC;"
+    
+    df_agg = pl.read_database(query, _conn)
+    
+    df_agg = df_agg.rename({
+        "fetched_at": "Time",
+        "total_bikes_av": "Total Bikes Available",
+        "total_docks_av": "Total Docks Available",
+        "empty_stations": "Empty Stations",
+        "full_stations": "Overflow Stations" 
+    })
+    return df_agg
+
+@st.cache_data(ttl=300) 
+def load_station_history(_conn):
+    query = """
+        SELECT name, bikes_av, fetched_at
+        FROM station_status_log
+        WHERE fetched_at IN (
+            SELECT DISTINCT fetched_at
+            FROM station_status_log
+            ORDER BY fetched_at DESC
+            LIMIT 18
+        );
+    """
+    
+    df_hist = pl.read_database(query, _conn)
+    
+    df_hist = df_hist.rename({
+        "fetched_at": "update_time",
+        "bikes_av": "number available bikes"
+    })
+    return df_hist
 
 
 st.write("""
@@ -19,28 +87,28 @@ To handle the surge, the network has added extra drop-off stations and more staf
 Let's see how the system holds up! 🚲
 """)
 
-# read 1031 most recent entries only (bottom up + header)
-n = 1031
-csv_path = "./data/output.csv"
+conn = get_db_connection()
+if conn is None:
+    st.stop()
 
-with open(csv_path, "r") as f:
-    header = f.readline().strip()
-    last_lines = deque(f, maxlen=n)
+df = load_latest_snapshot(conn)
+df_agg = load_aggregate_history(conn)
 
-csv_data = "\n".join([header] + [line.strip() for line in last_lines])
-df = pl.read_csv(csv_data.encode(), has_header=True) 
+if df.is_empty() or df_agg.is_empty():
+    st.warning("No data found in the database. Please run the fetcher scripts.")
+    st.stop()
 
-df_agg = pl.read_csv("./data/aggregate.csv", has_header=True) 
+
+
 a, b, c = st.columns([1, 1.2, 1.2])
 
-# 1. System-Wide Availability; "Total Bikes: 4,520 / 10,000"
+# 1. System-Wide Availability
 bikes_available = df_agg.select(pl.last('Total Bikes Available')).item()
 bikes_available_prev_update = df_agg.select(pl.col('Total Bikes Available')).tail(2).head(1).item()
 delta_bikes_av = bikes_available - bikes_available_prev_update
 a.metric("Total Bikes Available", f"{millify(bikes_available)} / {millify(TOTAL_BIKES)}", f'{delta_bikes_av}', border=True) 
 
-
-# 2. "System Stress (Empty)" (A metric: "Stations at < 3 bikes: 120") 
+# 2. "System Stress (Empty)"
 num_stations_less_3bikes = df_agg.select(pl.last('Empty Stations')).item()
 total_stations = df.select(pl.len()).item()
 percentage_deserts = num_stations_less_3bikes / total_stations * 100
@@ -48,7 +116,7 @@ empty_prev = df_agg.select(pl.col('Empty Stations')).tail(2).head(1).item()
 delta_empty = num_stations_less_3bikes - empty_prev
 b.metric("Stations at < 3 bikes", f"{num_stations_less_3bikes} ({percentage_deserts:.2f} %)", f"{delta_empty}", border=True)
 
-# 3. "System Stress (Full)" (A metric: "Stations at < 3 docks: 85")
+# 3. "System Stress (Full)"
 num_stations_less_3docks = df_agg.select(pl.last('Overflow Stations')).item()
 percentage_overflows = num_stations_less_3docks / total_stations * 100
 full_prev = df_agg.select(pl.col('Overflow Stations')).tail(2).head(1).item()
@@ -56,14 +124,14 @@ delta_full = num_stations_less_3docks - full_prev
 c.metric("Stations at < 3 docks", f"{num_stations_less_3docks} ({percentage_overflows:.2f} %)", f"{delta_full}", border=True)
 
 
+# map
 df_filtered = df.filter(
-    (pl.col("is functional") == 1) &
+    (pl.col("is functional") == True) & 
     (pl.col("lat") >= MIN_LAT) &
     (pl.col("lat") <= MAX_LAT) &
     (pl.col("lon") >= MIN_LON) &
     (pl.col("lon") <= MAX_LON)
 )
-
 
 data = df_filtered.to_pandas()
 
@@ -82,11 +150,10 @@ layer = pdk.Layer(
     data=data,
     get_position=["lon", "lat"],
     get_fill_color="color",
-    get_radius=30, # extra: decrease on zoom, increase on dezoom
+    get_radius=30, 
     pickable=True,
     opacity=0.8,
 )
-
 
 view_state = pdk.ViewState(latitude=45.4996, longitude=-73.5668, zoom=13)
 
@@ -94,42 +161,14 @@ deck = pdk.Deck(
     layers=[layer],
     initial_view_state=view_state,
     map_style="light",  
-    
     tooltip={
         'html': '{name} <br> Bikes: {number available bikes} <br> Open docks: {number available docks}',
-        'style': {
-            'color': 'white'
-        }
+        'style': {'color': 'white'}
     } # type: ignore
 )
 
-# reads last 18 updates (6 hours) for all stations
-def load_station_history():
-    num_stations = 1031
-    num_updates = 18
-    num_lines = num_stations * num_updates
 
-    csv_path = "./data/output.csv"
-    txt_path = "./data/update_time.txt"
-
-    # Read recent data from CSV
-    with open(csv_path, "r") as f:
-        header = f.readline().strip()
-        last_lines = deque(f, maxlen=num_lines)
-    csv_data = "\n".join([header] + [line.strip() for line in last_lines])
-    df = pl.read_csv(StringIO(csv_data), has_header=True)
-
-    # Read timestamps
-    with open(txt_path, "r") as f:
-        time_lines = [line.strip() for line in deque(f, maxlen=num_updates)]
-
-    df = df.with_columns(
-        pl.Series("update_time", time_lines * num_stations)
-    )
-
-    return df
-
-station_history_df = load_station_history().to_pandas()
+station_history_df = load_station_history(conn).to_pandas()
 station_names = sorted(station_history_df["name"].unique())
 
 col_map, col_chart = st.columns([2, 1])
@@ -138,7 +177,7 @@ with col_map:
     st.pydeck_chart(deck)
 
 with col_chart:
-    st.write("#### Bikes at Station (Last 12 Hours)")
+    st.write("#### Bikes at Station (Last ~12 Hours)")
     selected_station = st.selectbox("Select a station", station_names)
     if selected_station:
         station_df = station_history_df[station_history_df["name"] == selected_station]
@@ -149,22 +188,21 @@ with col_chart:
         station_chart_df = station_df[["time_label", "number available bikes"]].set_index("time_label")
         
         st.line_chart(
-        station_chart_df,
-        width='stretch',
-        height=250
-)
+            station_chart_df,
+            width='stretch',
+            height=250
+        )
 
 st.write("## Global Evolution")
 
-df_agg = df_agg.with_columns(pl.col("Time").str.strip_chars().str.to_datetime("%Y-%m-%d %H:%M:%S")).to_pandas()
-df_agg = df_agg.set_index("Time")
+df_agg_pd = df_agg.to_pandas()
+df_agg_pd = df_agg_pd.set_index("Time")
 
 st.write("### Station Stress")
-st.line_chart(df_agg[["Empty Stations", "Overflow Stations"]],
+st.line_chart(df_agg_pd[["Empty Stations", "Overflow Stations"]],
               width='stretch', color=['#FF0000', '#FFC800'],
               )
 
 st.write("### Bikes and Docks")
-st.line_chart(df_agg[["Total Bikes Available", "Total Docks Available"]],
+st.line_chart(df_agg_pd[["Total Bikes Available", "Total Docks Available"]],
               width='stretch')
-
---
+++ lambda.py
@@ -0,0 +1,145 @@
+def main():
+    import requests
+    import datetime as dt
+    import polars as pl
+    from typing import Tuple
+
+    from env_var import ENDPOINT, PW, USERNAME, NAME, PORT
+    import psycopg2
+    import psycopg2.extras
+    
+    def fetch(): 
+        bool = None
+        
+        station_info_url = 'https://gbfs.velobixi.com/gbfs/2-2/en/station_information.json'
+        info_response = requests.get(station_info_url)
+        stations_info_list = info_response.json()['data']['stations']
+        info_update_time = info_response.json()['last_updated']
+
+        station_status_url = 'https://gbfs.velobixi.com/gbfs/2-2/en/station_status.json'
+        status_response = requests.get(station_status_url)
+        stations_status_list = status_response.json()['data']['stations']
+
+        try:
+            df_info = pl.DataFrame(stations_info_list)
+            df_status = pl.DataFrame(stations_status_list)
+
+            df_merged = df_info.join(df_status, on="station_id", how="inner")
+
+            df = df_merged.select(
+                pl.col("name"),
+                pl.col("lat").fill_null(0.0), 
+                pl.col("lon").fill_null(0.0), 
+                pl.col("capacity"),
+                pl.col("num_bikes_available").alias("bikes_av"),
+                pl.col("num_docks_available").alias("docks_av"),
+                (
+                    (pl.col("is_installed") == 1) & 
+                    (pl.col("is_renting") == 1) & 
+                    (pl.col("is_returning") == 1)
+                )
+                .alias("is_functional")
+                .fill_null(False) 
+            )
+        except Exception as e:
+            print(f"Error processing data: {e}")
+            return False
+
+        try:
+            fetch_timestamp = dt.datetime.fromtimestamp(info_update_time)
+        except NameError:
+            print("'info_update_time' not av. Using current time ")
+            fetch_timestamp = dt.datetime.now() 
+
+        conn = None
+        try:
+            conn = psycopg2.connect(dbname=NAME, user=USERNAME, password=PW, host=ENDPOINT)
+        except (Exception, psycopg2.Error) as error:
+            print(f"Error connecting to database: {error}")
+            return False
+
+        if conn:
+            command = """
+                INSERT INTO station_status_log
+                (name, lat, lon, capacity, bikes_av, docks_av, is_functional, fetched_at) 
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s); 
+                """
+            
+            data_to_insert = [row + (fetch_timestamp,) for row in df.rows()]
+            
+            try:
+                with conn:
+                    with conn.cursor() as curs:
+                        psycopg2.extras.execute_batch(curs, command, data_to_insert)
+                        print(f"Successfully inserted {len(data_to_insert)} rows.")
+                        bool = True
+                            
+            except (Exception, psycopg2.Error) as error:
+                print(f"Error during insert: {error}")
+                bool = False
+            
+            finally:
+                if conn is not None:
+                    conn.close()
+                    print("Database connection closed.")
+                    return bool
+    
+        def aggregate():
+            conn = None
+            
+            try:
+                conn = psycopg2.connect(dbname=NAME, user=USERNAME, password=PW, host=ENDPOINT)
+            except (Exception, psycopg2.Error) as error:
+                print(f"Error connecting to database: {error}")
+                return 
+                
+            if conn:
+                try:
+                    with conn:
+                        with conn.cursor() as curs:
+                    
+                            curs.execute("SELECT MAX(fetched_at) FROM station_status_log;")
+                            row = curs.fetchone()
+                        
+                            if not (row and row[0]):
+                                print("No data found in station_status_log.")
+                                return 
+                                
+                            most_recent = row[0]
+                            print(f"Processing data for timestamp: {most_recent}")
+
+                            aggregate_command = """
+                                SELECT
+                                    SUM(bikes_av),
+                                    SUM(docks_av),
+                                    COUNT(*) FILTER (WHERE bikes_av < 3),
+                                    COUNT(*) FILTER (WHERE docks_av < 3)
+                                FROM station_status_log
+                                WHERE fetched_at = %s;
+                            """
+                            curs.execute(aggregate_command, (most_recent,))
+                            agg_row = curs.fetchone()
+                    
+                            if not agg_row:
+                                print(f"Could not fetch aggregates for {most_recent}.")
+                                raise Exception("Aggregate query returned no rows")
+
+                            insert_command = """
+                                INSERT INTO system_aggregate_log
+                                (total_bikes_av, total_docks_av, empty_stations, full_stations, fetched_at)
+                                VALUES (%s, %s, %s, %s, %s)
+                            """
+                            vals = agg_row + (most_recent,)
+                            
+                            curs.execute(insert_command, vals)
+                            print("Successfully inserted aggregate data.")
+                                
+                except (Exception, psycopg2.DatabaseError) as error:
+                    print(f"Database error occurred: {error}. Transaction will be rolled back.")
+                finally:
+                    if conn is not None:
+                        conn.close()
+                        print("Database connection closed.")
+                        
+        if fetch():
+            aggregate()
